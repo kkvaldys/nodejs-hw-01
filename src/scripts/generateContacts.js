ac9b2b0902f67c4c5386c0f893b8d25795c9e731
kkvaldys <vladbigun677@gmail.com>
2024-06-21 17:34:11 +0300

    created

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,8 @@
 import { PATH_DB } from '../constants/contacts.js';
+import createFakeContact from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const contact = {};
+};
 
 generateContacts(5);
